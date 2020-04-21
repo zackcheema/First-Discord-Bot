75f6523d65c011a802b4aff615e6ba8dc68cbe8b
Zachary <Zachary.cheem@gmail.com>
2020-04-21 17:19:02 -0230

    Fixed small changes, added the ! prefix, and added the help command

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
+const PREFIX = '!';
+
 bot.on('ready', () => {
     console.log('This bot is running!');
 })
 
 bot.on('message', message => {
-    let args = message.content.substring(process.env.PREFIX.length).split(" ");
+    let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
         case 'sheet':
@@ -17,6 +19,9 @@ bot.on('message', message => {
         break;
         case 'form':
             message.reply(process.env.FORM);
+        break;
+        case 'help':
+            message.reply("Below are the available commands \n!sheet will send a link to the civ 6 spreadsheet \n!form will send a link to the form");
         break;
     }
 })
